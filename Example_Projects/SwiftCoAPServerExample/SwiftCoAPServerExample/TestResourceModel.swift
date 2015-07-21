df021faa4733cd8ca072dc3ee1fcd91f6774c55c
wojkord <wojkord@tzi.de>
2015-07-21 15:59:40 +0200

    shortened enum calls

--- Example_Projects/SwiftCoAPServerExample/SwiftCoAPServerExample/TestResourceModel.swift
+++ Example_Projects/SwiftCoAPServerExample/SwiftCoAPServerExample/TestResourceModel.swift
@@ -24,27 +24,27 @@ class TestResourceModel: SCResourceModel {
     }
     
     override func dataForGet(queryDictionary queryDictionary: [String : String], options: [Int : [NSData]]) -> (statusCode: SCCodeValue, payloadData: NSData?, contentFormat: SCContentFormat!)? {
-        return (SCCodeValue(classValue: 2, detailValue: 05)!, myText.dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain)
+        return (SCCodeValue(classValue: 2, detailValue: 05)!, myText.dataUsingEncoding(NSUTF8StringEncoding), .Plain)
     }
     
     override func dataForPost(queryDictionary queryDictionary: [String : String], options: [Int : [NSData]], requestData: NSData?) -> (statusCode: SCCodeValue, payloadData: NSData?, contentFormat: SCContentFormat!, locationUri: String!)? {
         if let data = requestData, string = NSString(data: data, encoding: NSUTF8StringEncoding) as? String{
             myText = string
-            return (SCCodeSample.Created.codeValue(), "Data created successfully".dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain, self.name)
+            return (SCCodeSample.Created.codeValue(), "Data created successfully".dataUsingEncoding(NSUTF8StringEncoding), .Plain, self.name)
         }
-        return (SCCodeSample.Forbidden.codeValue(), "Invalid Data sent".dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain, nil)
+        return (SCCodeSample.Forbidden.codeValue(), "Invalid Data sent".dataUsingEncoding(NSUTF8StringEncoding), .Plain, nil)
     }
     
     override func dataForPut(queryDictionary queryDictionary: [String : String], options: [Int : [NSData]], requestData: NSData?) -> (statusCode: SCCodeValue, payloadData: NSData?, contentFormat: SCContentFormat!, locationUri: String!)? {
         if let data = requestData, string = NSString(data: data, encoding: NSUTF8StringEncoding) as? String{
             myText += string
-            return (SCCodeSample.Changed.codeValue(), "Update Successful".dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain, self.name)
+            return (SCCodeSample.Changed.codeValue(), "Update Successful".dataUsingEncoding(NSUTF8StringEncoding), .Plain, self.name)
         }
-        return (SCCodeSample.Forbidden.codeValue(), "Invalid Data sent".dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain, nil)
+        return (SCCodeSample.Forbidden.codeValue(), "Invalid Data sent".dataUsingEncoding(NSUTF8StringEncoding), .Plain, nil)
     }
     
     override func dataForDelete(queryDictionary queryDictionary: [String : String], options: [Int : [NSData]]) -> (statusCode: SCCodeValue, payloadData: NSData?, contentFormat: SCContentFormat!)? {
         myText = ""
-        return (SCCodeSample.Delete.codeValue(), "Deleted".dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain)
+        return (SCCodeSample.Delete.codeValue(), "Deleted".dataUsingEncoding(NSUTF8StringEncoding), .Plain)
     }
 }
--- Example_Projects/SwiftCoAPServerExample/SwiftCoAPServerExample/TimeResourceModel.swift
+++ Example_Projects/SwiftCoAPServerExample/SwiftCoAPServerExample/TimeResourceModel.swift
@@ -36,6 +36,6 @@ class TimeResourceModel: SCResourceModel {
     }
     
     override func dataForGet(queryDictionary queryDictionary: [String : String], options: [Int : [NSData]]) -> (statusCode: SCCodeValue, payloadData: NSData?, contentFormat: SCContentFormat!)? {
-        return (SCCodeValue(classValue: 2, detailValue: 05)!, myText.dataUsingEncoding(NSUTF8StringEncoding), SCContentFormat.Plain)
+        return (SCCodeValue(classValue: 2, detailValue: 05)!, myText.dataUsingEncoding(NSUTF8StringEncoding), .Plain)
     }
 }

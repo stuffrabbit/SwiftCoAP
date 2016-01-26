2f54b08d9225814116c27be5e1170b39b482163d
Ben Flannery <oflannabhra@gmail.com>
2016-01-26 15:49:34 -0500

    added Carthage support

---
+++ SwiftCoAP/SwiftCoAP.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCoAP.
+FOUNDATION_EXPORT double SwiftCoAPVersionNumber;
+
+//! Project version string for SwiftCoAP.
+FOUNDATION_EXPORT const unsigned char SwiftCoAPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCoAP/PublicHeader.h>
+
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>

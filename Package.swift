732250c19c256ac13f7ef39ca80c0546c46ef9e4
Ilya <uin153974748@gmail.com>
2021-09-23 15:32:40 +1000

    Fix exclude for Xcode 13 (#12)

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftCoAP",
-            dependencies: [],
-            exclude: ["Example_Projects"]),
+            dependencies: []),
         .testTarget(
             name: "SwiftCoAPTests",
             dependencies: ["SwiftCoAP"]),
